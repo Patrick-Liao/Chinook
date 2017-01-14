db4f505e467e720f8f438f53ea24699e710a8ced
Alex Fang <frjalex@gmail.com>
2017-01-14 15:56:34 -0500

    sdf

---
+++ cmake-build-debug/CMakeFiles/chinookTest1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for chinookTest1.
+# This may be replaced when dependencies are built.
---
+++ src/ChinookTest.h
@@ -0,0 +1,22 @@
+//
+// Created by explorer on 1/14/17.
+//
+
+#ifndef CHINOOKTEST1_CHINOOKTEST_H
+#define CHINOOKTEST1_CHINOOKTEST_H
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <IterativeRobot.h>
+#include <LiveWindow/LiveWindow.h>
+#include <SmartDashboard/SendableChooser.h>
+#include <SmartDashboard/SmartDashboard.h>
+
+class ChinookTest {
+
+};
+
+
+#endif //CHINOOKTEST1_CHINOOKTEST_H
