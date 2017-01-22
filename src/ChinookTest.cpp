29fde0f9e5ae29f08710bbc7ffa13bc4730cb25a
tylerliu <tylersiqi@163.com>
2017-01-21 21:14:53 -0500

    Update ChinookTest.cpp

--- src/ChinookTest.cpp
+++ src/ChinookTest.cpp
@@ -154,7 +154,7 @@ public:
 
     void arcadeDrive(double x, double y) {
 
-        drive->ArcadeDrive(-(std::abs(y) >= speedLimit ? (y/std::abs(y)) * speedLimit : y),
+        drive->ArcadeDrive(-(y/std::abs(y)) * (speedLimit > std::abs(y) ? speedLimit : std::abs(y))),
                            -(((std::abs(std::abs(y) >= 0.5 ? x * 0.6 : x)) >= speedLimit) ?
                                    (((std::abs(y) >= 0.5 ? x * 0.6 : x) < 0) ? -1 : 1) * speedLimit :
                                    (std::abs(y) >= 0.5 ? x * 0.6 : x)));
