c1d68bd04bca2db20fbe7add6247b961636a550b
Patrick_Liao <lbypatrick.public@gmail.com>
2017-02-04 14:58:59 -0500

    Spliting ChinookTest.cpp #5

---
+++ src/statics.h
@@ -0,0 +1,39 @@
+#include "opState.h"
+
+enum {
+    // wheels
+    LeftPortFront = 0,
+    LeftPortBack = 2,
+    RightPortFront = 1,
+    RightPortBack = 3,
+    // Ball pick upx
+    LeftBall = 4,
+    RightBall = 5,
+    // Gamepad Port Mappings
+    GamepadPort = 3,
+    GamepadAButton = 1,
+    GamepadBButton = 2,
+    GamepadXButton = 3,
+    GamepadYButton = 4,
+    GamepadLeftBumper = 5,
+    GamepadRightBumper = 6,
+    GamepadBack = 7,
+    GamepadStart = 8,
+    GamepadLeftStickDown = 1,
+    GamepadRightStickDown = 10,
+    JoystickTrigger = 1,
+    // Max Power
+    MaxPower = 2,
+    // Axis
+    LeftStickX = 0,
+    LeftStickY = 1,
+    LeftTrigger = 2,
+    RightTrigger = 3,
+    RightStickX = 4,
+    RightStickY = 5,
+    // Joystick
+    JoystickPort = 3,
+    JoystickAButton = 3,
+    JoystickYButton = 4,
+    JoystickLeftStickDown = 1
+};
