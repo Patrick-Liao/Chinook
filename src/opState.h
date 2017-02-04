7137500060bcec99ac0e6af21b640c0be6975216
Patrick_Liao <lbypatrick.public@gmail.com>
2017-02-04 14:14:54 -0500

    aRacingBoat

--- src/opState.h
+++ src/opState.h
@@ -2,6 +2,32 @@
 #define OPSTATE_H
 #include "ChinookTest.h"
 
+struct {
+    // button
+    bool jsIsAButtonPressed = false;
+    bool gpIsBButtonPressed = false;
+    bool gpIsXButtonPressed = false;
+    bool jsIsYButtonPressed = false;
+    bool gpIsLeftBumperPressed = false;
+    bool gpIsRightBumperPressed = false;
+    bool gpIsBackPressed = false;
+    bool gpIsStartPressed = false;
+    bool jsIsLeftStickDown = false;
+    bool gpIsRightStickDown = false;
+    bool jsIsTriggered = false;
+
+    // axis
+    double jsLeftStickX = .0;
+    double jsLeftStickY = .0;
+    double gpLeftTrigger = .0;
+    double gpRightTrigger = .0;
+    double gpRightStickX = .0;
+    double gpRightStickY = .0;
+
+    // DPAD
+    double gpDpad = -1.0;
+} OpState;
+
 void setOpState();
 
 void resetOpState();
